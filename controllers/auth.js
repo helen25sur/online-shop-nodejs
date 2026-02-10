dba60cf7a364cb109702566c1ca6fb9573ab91db
Olena Surilova <elen21surilova@gmail.com>
2026-02-10 21:04:04 +0100

    feat: add cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get('Cookie').split(';')[5].trim().split('=')[1] === 'true';
   res.render('auth/login', {
     pageTitle: 'Login',
     path: '/login',
+    isAuthenticated: isLoggedIn
   });
+}
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie', 'loggedIn=true; Max-Age=100; HttpOnly'); // Expires in 100 seconds. HttpOnly means that the cookie cannot be accessed by client-side JavaScript, which helps to prevent cross-site scripting (XSS) attacks.
+  res.redirect('/');
 }
--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const authControllers = require('../controllers/auth');
 
 router.get('/login', authControllers.getLogin);
+router.post('/login', authControllers.postLogin);
 
 module.exports = router;
