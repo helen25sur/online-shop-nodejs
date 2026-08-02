632eb3f93ae13e3e9e9478b315eb80315354e4f3
Olena Surilova <elen21surilova@gmail.com>
2026-08-02 16:50:43 +0200

    feat: create functionality to register user

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,6 +15,7 @@ exports.postLogin = (req, res, next) => {
     .then(user => {
       if (!user) {
         // такого email немає в БД
+        console.log('User not found');
         return res.redirect('/login');
       }
       return bcryptjs.compare(password, user.password)
@@ -28,6 +29,7 @@ exports.postLogin = (req, res, next) => {
             });
           }
           // пароль неправильний
+          console.log('Incorrect password');
           res.redirect('/login');
         });
     })
@@ -39,4 +41,37 @@ exports.postLogout = (req, res, next) => {
     if (err) console.log(err);
     res.redirect('/');
   });
+};
+
+exports.getRegister = (req, res, next) => {
+  res.render('auth/register', {
+    pageTitle: 'Register',
+    path: '/register'
+  });
+};
+
+exports.postRegister = (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  User.findOne({ where: { email: email } })
+    .then(user => {
+      if (user) {
+        // користувач з таким email вже існує
+        console.log('User already exists');
+        return res.redirect('/register');
+      }
+      return bcryptjs.hash(password, 12)
+        .then(hashedPassword => {
+          return User.create({
+            name: username,
+            email: email,
+            password: hashedPassword
+          });
+        })
+        .then(result => {
+          console.log(result.dataValues);
+          res.redirect('/login');
+        });
+    })
+    .catch(err => console.error(err));
 };
--- routes/auth.js
+++ routes/auth.js
@@ -6,5 +6,7 @@ const authControllers = require('../controllers/auth');
 router.get('/login', authControllers.getLogin);
 router.post('/login', authControllers.postLogin);
 router.post('/logout', authControllers.postLogout);
+router.get('/register', authControllers.getRegister);
+router.post('/register', authControllers.postRegister);
 
 module.exports = router;
